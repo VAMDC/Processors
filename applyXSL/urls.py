2c289d53cf98fd4920607d379ebf6d00b180dc48
Thomas Marquart <thomas@marquart.se>
2012-02-21 11:47:01 +0100

    add templates for /availability and /capabilties and make them work

--- applyXSL/urls.py
+++ applyXSL/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('applyXSL.views',
     (r'^(?P<xsl>\w+)/$', 'receiveData'),
     (r'^(?P<xsl>\w+)/service$', 'receiveData'),
-    (r'^(?P<xsl>\w+)/result/(?P<rid>\w+)/$', 'deliverResult')
-    (r'^(?P<xsl>\w+)/availability$', direct_to_template, {'template':'availability.xml'} )
+    (r'^(?P<xsl>\w+)/result/(?P<rid>\w+)/$', 'deliverResult'),
+    (r'^(?P<xsl>\w+)/availability$', direct_to_template, {'template':'availability.xml'} ),
     (r'^(?P<xsl>\w+)/capabilities$', direct_to_template, {'template':'capabilities.xml'})
     )
